caa5ff4f5a1567d47a92a4a7c994de8865a6519d
Nicolas Dolinkue <dolinkue_n@hotmail.com>
2022-11-29 11:33:16 -0300

    log in and register

---
+++ chatTecoNico/Controllers/ConversationViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  chatTecoNico
+//
+//  Created by Nicolas Dolinkue on 28/11/2022.
+//
+
+import UIKit
+
+class ConversationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let isLoggedIn = UserDefaults.standard.bool(forKey: "logged_in")
+        
+        if !isLoggedIn {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: false)
+        }
+        
+        
+    }
+
+
+}
+
